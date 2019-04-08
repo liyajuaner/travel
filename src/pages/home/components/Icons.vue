996fd308892f8fee0533d9de8927d6407c8a7a1f
liyajuaner <1021803856@qq.com>
2019-04-08 15:50:09 +0800

    add index-recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -35,8 +35,8 @@ export default {
         desc: '动植园'
       }, {
         id: '0005',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
-        desc: '动植园'
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
       }, {
         id: '0006',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
@@ -49,10 +49,6 @@ export default {
         id: '0008',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
         desc: '动植园'
-      }, {
-        id: '0009',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
-        desc: '一日游'
       }]
     }
   },
@@ -79,37 +75,39 @@ export default {
     width: 100%
     height: 0
     padding-bottom: 50%
-  .icon 
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img 
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: 0.44rem
-      box-sizing: border-box
-      padding: 0.1rem
-      .icon-img-content 
-        height: 100%
-        display: block
-        margin: 0 auto
-    .icon-desc 
-      text-align: center
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: 0.44rem
-      line-height: 0.44rem
-      color: $darkTextColor
-      overflow:hidden
-      wrap:nowrap
-      text-overflow:ellipsis
+  .icons
+    margin-top:.1rem
+    .icon 
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img 
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: 0.44rem
+        box-sizing: border-box
+        padding: 0.1rem
+        .icon-img-content 
+          height: 100%
+          display: block
+          margin: 0 auto
+      .icon-desc 
+        text-align: center
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: 0.44rem
+        line-height: 0.44rem
+        color: $darkTextColor
+        overflow:hidden
+        wrap:nowrap
+        text-overflow:ellipsis
 </style>
    
 
