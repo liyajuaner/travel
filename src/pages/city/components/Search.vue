2b36b771876b63a5af2658d9dd52b4f660a4777b
liyajuaner <1021803856@qq.com>
2019-04-10 16:51:14 +0800

    add city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="请输入城市名或拼音" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+
+  .search-input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    padding: 0 0.1rem;
+    border-radius: 0.06rem;
+    text-align: center;
+    color: #666;
+  }
+}
+</style>
+
+
