b7ab2668fc492c03ae15a319acc2a47df8444afb
liyajuaner <1021803856@qq.com>
2019-04-11 17:11:08 +0800

    add city-ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,73 +1,41 @@
 <template>
-    <div class="list" ref="wrapper">
-        <div>
-            <div class="area">
-                <div class="title border-topbottom">当前城市</div>
-                <div class="button-list">
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbottom">热门城市</div>
-                <div class="button-list">
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbottom">A</div>
-                <ul class="item-list">
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                </ul>
-                <div class="title border-topbottom">A</div>
-                <ul class="item-list">
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                </ul>
-                <div class="title border-topbottom">A</div>
-                <ul class="item-list">
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                </ul>
-            </div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{ item.name }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <ul class="item-list">
+          <li class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+            {{innerItem.name}}
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import Bscroll from "better-scroll";
 export default {
   name: "CityList",
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted() {
     this.scroll = new Bscroll(this.$refs.wrapper);
   }
