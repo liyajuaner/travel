7c4553dc4d3bddaa064b1873c53c28192d475c45
liyajuaner <1021803856@qq.com>
2019-04-12 16:10:17 +0800

    add city-search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,66 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="请输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input
+        v-model="keyword"
+        type="text"
+        class="search-input"
+        placeholder="请输入城市名或拼音"
+      />
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item boder-bottom" v-for="item of list" :key=item.id>{{item.name}}</li>
+        <li class="search-item boder-bottom" v-for="item of list" :key=item.id v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name:'CitySearch'
-};
+  name: "CitySearch",
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer:null
+    }
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1 ) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      },100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
+}
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
@@ -15,7 +69,6 @@ export default {
   height: 0.72rem;
   padding: 0 0.1rem;
   background: $bgColor;
-
   .search-input {
     box-sizing: border-box;
     width: 100%;
@@ -27,6 +80,20 @@ export default {
     color: #666;
   }
 }
+  .search-content
+    z-index:1
+    overflow:hidden
+    position:absolute
+    top:1.58rem
+    left:0
+    right:0
+    bottom:0
+    background:#eee
+    .search-item
+      line-height:.62rem
+      padding-left:.2rem
+      background:#fff
+      color:#666
 </style>
 
 
