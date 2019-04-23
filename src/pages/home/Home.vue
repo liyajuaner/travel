b04f18ca4546912215db955647c2d9a0f4e7d7c2
liyajuaner <1021803856@qq.com>
2019-04-23 20:52:32 +0800

    修改axios

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -39,7 +39,7 @@ export default {
     methods:{
         getHomeInfo () {
             
-            axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
+            axios.get('http://39.106.151.76/resume/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc (res) {
             res = res.data
